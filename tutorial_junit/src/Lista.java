08330e97405d20c339ac914da25fa0901210fbc0
bianca <bian.lunabian@gmail.com>
2025-09-24 23:01:15 +0200

    Update tutorial and first session
    Tutorial of how to use JUnit in IntelliJ and first touch of testing within the first session.

---
+++ tutorial_junit/src/Lista.java
@@ -0,0 +1,31 @@
+public class Lista {
+
+  public boolean afegirUltim(int valor){
+    return true;
+  };
+
+  public boolean insertarValor(int posicio, int valor) {
+    return false;
+  }
+
+  public boolean eliminaValor(int posicio) {
+    return false;
+  }
+
+  public int getValor(int posicio) {
+    return 0;
+  }
+
+  public boolean esBuida() {
+    return false;
+  }
+
+  public int getNElements() {
+    return 0;
+  }
+
+  private Node primer;
+
+}
+
+
---
+++ tutorial_junit/src/Node.java
@@ -0,0 +1,26 @@
+public class Node {
+
+  public Node() {
+
+  }
+
+  public Node(int v) {
+
+  }
+
+  public void setValor(int v) {
+
+  }
+
+  public Node getNext() {
+    return null;
+  }
+
+  public void setNext(Node next) {
+
+  }
+
+  private int valor;
+  private Node next;
+
+}
